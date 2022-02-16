3f8d153031c6bcd2c837400d99ff31c04095318c
gizemarik <zgizem.arik@gmail.com>
2022-02-17 00:37:22 +0300

    To Do List trial app created

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter an item"
+          style={styles.input}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+  }
+});
